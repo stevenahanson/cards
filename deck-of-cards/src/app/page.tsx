e7dbf8de6b1c0856211ae4317be590877441abb6
Steven Hanson <stevenahanson@live.com>
2025-09-01 09:42:22 +0100

    get deck of cards and store deck id

--- deck-of-cards/src/app/page.tsx
+++ deck-of-cards/src/app/page.tsx
@@ -1,3 +1,34 @@
+"use client";
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return <h1>hello world</h1>;
+  const [deckId, setDeckId] = useState([]);
+
+  useEffect(() => {
+    async function getCards() {
+      const url =
+        "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+
+      try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        setDeckId(result.deck_id);
+      } catch (error) {
+        if (error instanceof Error) {
+          console.error(error.message);
+        } else {
+          console.error(String(error));
+        }
+      }
+    }
+
+    getCards();
+  }, []);
+
+  return <>{deckId && <h2>Deck ready</h2>}</>;
 }
