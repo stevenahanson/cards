86010ba1d19fd6c4118acf6d3364feadc68e3c63
Steven Hanson <stevenahanson@live.com>
2025-09-01 10:56:41 +0100

    display cards + add card styling

--- deck-of-cards/src/app/page.tsx
+++ deck-of-cards/src/app/page.tsx
@@ -44,18 +44,35 @@ export default function Home() {
           <div>
             <h2 className="uppercase py-4">some text!</h2>
           </div>
-          <div className="flex space-x-6 text-white">
-            {cards.length > 0 && (
-              <div className="py-10">
+
+          {/* Cards */}
+          <div className="flex space-x-6 text-white py-10">
+            {/* Previous card */}
+            <div className="w-24 h-34 border-2 border-black rounded-sm flex items-center justify-center ">
+              {cards.length > 1 && (
+                <Image
+                  src={cards[cards.length - 2].image}
+                  width={100}
+                  height={150}
+                  alt="Previous card"
+                  className="object-contain"
+                />
+              )}
+            </div>
+            {/* Current card */}
+            <div className="w-24 h-34 border-2 border-black rounded-sm flex items-center justify-center">
+              {cards.length > 0 && (
                 <Image
                   src={cards[cards.length - 1].image}
                   width={100}
-                  height={100}
+                  height={150}
                   alt="Current card"
+                  className="object-contain"
                 />
-              </div>
-            )}
+              )}
+            </div>
           </div>
+
           <button
             onClick={handleDraw}
             className="bg-blue-400 text-white p-2 rounded-md border-2 border-blue-500"
